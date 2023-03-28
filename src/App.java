0bb5371201932aebb9887262e2766862c9e2ed23
lgm_12345 <lucasgugelmaciel@gmail.com>
2023-03-28 11:16:37 -0300

    comit inicial

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Veiculo veiculo = new Veiculo("BRA2E19","Ford" "Focus" ) ;
+        Cliente cliente = new Cliente("Lucas","052.700.529-03","07/01/2000",23,"Campinas");
+    }
+}
